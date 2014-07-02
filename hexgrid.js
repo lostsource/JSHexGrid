ead64229384351e57761a78b9abd2d47cb85101a
Joseph Portelli <joseph@lostsource.com>
2014-07-02 21:55:51 +0200

    Added license info

--- hexgrid.js
+++ hexgrid.js
@@ -1,3 +1,13 @@
+/**
+* This work is licensed under the 
+* Creative Commons Attribution-ShareAlike 3.0 Unported License. 
+* To view a copy of this license, visit 
+* http://creativecommons.org/licenses/by-sa/3.0/
+*
+* joseph@lostsource.com
+*
+**/
+
 "use strict";
 
 var JSHexGrid = (function(){
